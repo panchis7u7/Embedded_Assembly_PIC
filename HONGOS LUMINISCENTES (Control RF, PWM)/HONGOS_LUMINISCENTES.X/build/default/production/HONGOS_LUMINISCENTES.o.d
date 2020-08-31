f9e2cbe6adcde48148b6af2346575bab84dea77d
Panchis7u7 <smadrigal.rod@gmail.com>
2020-08-31 18:23:08 -0500

    Controlador de brillo en LED co pulsador (uso de interrupciones).

---
+++ HONGOS LUMINISCENTES (Control RF, PWM)/HONGOS_LUMINISCENTES.X/build/default/production/HONGOS_LUMINISCENTES.o.d
@@ -0,0 +1,3 @@
+build/default/production/HONGOS_LUMINISCENTES.o:  \
+	E:/Proyectos/PIC/HONGOS\ LUMINISCENTES\ (Control\ RF,\ PWM)/HONGOS_LUMINISCENTES.X/HONGOS_LUMINISCENTES.asm  \
+
--- LED (Parpadeo)/LED_PARPADEO.X/build/default/production/LED_PARPADEO.o.d
+++ LED (Parpadeo)/LED_PARPADEO.X/build/default/production/LED_PARPADEO.o.d
@@ -1,4 +1,3 @@
 build/default/production/LED_PARPADEO.o:  \
-	E:/PruebaPIC/LED_PARPADEO.X/LED_PARPADEO.asm  \
-	E:/PruebaPIC/LED_PARPADEO.X/RETARDOS.inc  \
+	E:/Proyectos/PIC/LED\ (Parpadeo)/LED_PARPADEO.X/LED_PARPADEO.asm  \
 
