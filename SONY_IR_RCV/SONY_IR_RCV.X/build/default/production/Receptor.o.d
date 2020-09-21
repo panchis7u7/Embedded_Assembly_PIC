de15f0b6917a139680416fb081c3e3130f876283
Panchis7u7 <smadrigal.rod@gmail.com>
2020-09-21 12:03:22 -0500

    LCD con impresion de variables hecho... Se procede a empezar a detectar los pulsos infrarojos de un control SONY.

---
+++ SONY_IR_RCV/SONY_IR_RCV.X/build/default/production/Receptor.o.d
@@ -0,0 +1,7 @@
+build/default/production/Receptor.o:  \
+	E:/Proyectos/PIC/SONY_IR_RCV/SONY_IR_RCV.X/Receptor.asm  \
+	E:/Proyectos/PIC/SONY_IR_RCV/SONY_IR_RCV.X/MACROS.inc  \
+	E:/Proyectos/PIC/SONY_IR_RCV/SONY_IR_RCV.X/RETARDOS.inc  \
+	E:/Proyectos/PIC/SONY_IR_RCV/SONY_IR_RCV.X/LCD20X4.inc  \
+	E:/Proyectos/PIC/SONY_IR_RCV/SONY_IR_RCV.X/BCD.inc  \
+
